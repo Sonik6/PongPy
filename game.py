cf3a00be1e8f02bd35a4e9d785cc49eb32b45957
Slawek Galka <galka.slawek6@gmail.com>
2021-01-31 16:00:05 +0100

    player class

--- game.py
+++ game.py
@@ -1,4 +1,6 @@
 import pygame, sys
+from player import Player
+
 pygame.init()
 
 #basic colors
@@ -9,16 +11,32 @@ blue = (54, 89, 239)
 
 #Open a window
 screenSize = (720, 360)
-screen = pygame.display.set_mode()
+screen = pygame.display.set_mode(screenSize)
 pygame.display.set_caption("Pong")
 
 
 isRunning = True
 clock = pygame.time.Clock()
 
-#----------Main Loop------------
+# ---------- Main Loop ------------
 while isRunning:
-    #Main event
+    # --- Main event loop
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             isRunning = False
+
+
+    # --- Game logic
+
+    # --- Drawing code
+    screen.fill(black)
+    pygame.draw.line(screen, white, [359, 0], [359, 360], 5)
+    
+    # --- Update
+    pygame.display.flip()
+
+    # --- Frame limit
+    clock.tick(60)
+
+
+pygame.quit
---
+++ player.py
@@ -0,0 +1,26 @@
+import pygame
+
+#basic colors
+white = (255, 255, 255)
+black = (0, 0, 0)
+red = (239, 34, 20)
+blue = (54, 89, 239)
+
+class Player(pygame.sprite.Sprite):
+    #This class represents a paddle. It derives from the "Sprite" class in Pygame.
+    
+    def __init__(self, color, width, height):
+        # Call the parent class (Sprite) constructor
+        super().__init__()
+        
+        # Pass in the color of the paddle, and its x and y position, width and height.
+        # Set the background color and set it to be transparent
+        self.image = pygame.Surface([width, height])
+        self.image.fill(black)
+        self.image.set_colorkey(black)
+ 
+        # Draw the paddle (a rectangle!)
+        pygame.draw.rect(self.image, color, [0, 0, width, height])
+        
+        # Fetch the rectangle object that has the dimensions of the image.
+        self.rect = self.image.get_rect()
